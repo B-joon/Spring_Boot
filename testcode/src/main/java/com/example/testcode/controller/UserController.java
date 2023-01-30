e896d8d003200975c09633b404c002746718fb57
B-joon <dosel45@gmail.com>
2023-01-30 18:01:00 +0900

    db 연결 설정 테스트

--- testcode/src/main/java/com/example/testcode/controller/UserController.java
+++ testcode/src/main/java/com/example/testcode/controller/UserController.java
@@ -1,28 +1,19 @@
 package com.example.testcode.controller;
 
 import com.example.testcode.entity.UserEntity;
-import com.example.testcode.repository.UserRepository;
 import com.example.testcode.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
-
     @Autowired
     private UserService userService;
-
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @GetMapping("/all")
     public List<UserEntity> searchAll() throws Exception {
--- testcode/src/main/java/com/example/testcode/entity/UserEntity.java
+++ testcode/src/main/java/com/example/testcode/entity/UserEntity.java
@@ -3,19 +3,17 @@ package com.example.testcode.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
-@Entity(name = "TEST_MEMBER")
+@Entity
 @Table(name = "TEST_MEMBER")
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, unique = true, name = "MEMBER_IDX")
     private long member_idx;
 
